02968ba9e7a5c956140615741afdcfa424d6da72
David Khauan <thalsmatsumira@gmail.com>
2024-09-24 13:11:54 -0300

    refatoração

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // Carregar o arquivo JSON com as notícias
-fetch('../noticias.json')
-  .then(response => response.json())
-  .then(data => {
+fetch ('../noticias.json')
+  .then (response => response.json())
+  .then (data => {
     // Identifica a categoria da página atual
-    const paginaAtual = window.location.pathname.split("/").pop().split(".")[0];
+    const paginaAtual = window.location.pathname.split ("/").pop().split (".") [0]
 
     // Verifica se há notícias para a categoria atual
-    if (data.noticias[paginaAtual]) {
-      const noticias = data.noticias[paginaAtual];
-      const container = document.querySelector('.main-content');
+    if (data.noticias [paginaAtual]) {
+      const noticias = data.noticias [paginaAtual]
+      const container = document.querySelector ('.main-content')
 
       // Gera os cards de notícias
-      noticias.forEach((noticia, index) => {
+      noticias.forEach ((noticia, index) => {
         const card = `
           <div class="card-noticia">
             <img src="${noticia.imagem}" alt="${noticia.titulo}">
@@ -20,10 +20,10 @@ fetch('../noticias.json')
             <a href="../pages/layout/noticia.html?id=${index}">Leia mais</a>
           </div>
         `;
-        container.innerHTML += card;
+        container.innerHTML += card
       });
     } else {
-      console.error('Nenhuma notícia encontrada para esta categoria.');
+      console.error ('Nenhuma notícia encontrada para esta categoria.')
     }
   })
-  .catch(error => console.error('Erro ao carregar o JSON:', error));
+  .catch (error => console.error ('Erro ao carregar o JSON:', error))
