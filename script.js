07998118bb54bf08b681dfbc5e993dea563d8dd7
David Khauan <thalsmatsumira@gmail.com>
2024-09-27 18:28:21 -0300

    mudança de conteudo

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-// Carregar o arquivo JSON com as notícias
 fetch ('../noticias.json')
   .then (response => response.json())
-  .then (data => {
-    // Identifica a categoria da página atual
-    const paginaAtual = window.location.pathname.split ("/").pop().split (".") [0]
+  .then (data => { 
+    const paginaAtual = window.location.pathname.split ('/').pop().split ('.') [0]
+    const container = document.querySelector ('.main-content')
 
-    // Verifica se há notícias para a categoria atual
     if (data.noticias [paginaAtual]) {
       const noticias = data.noticias [paginaAtual]
-      const container = document.querySelector ('.main-content')
 
-      // Gera os cards de notícias
-      noticias.forEach ((noticia, index) => {
-        const card = `
-          <div class="card-noticia">
-            <img src="${noticia.imagem}" alt="${noticia.titulo}">
-            <h2>${noticia.titulo}</h2>
-            <p>${noticia.descricao}</p>
-            <a href="../pages/layout/noticia.html?id=${index}">Leia mais</a>
-          </div>
-        `;
-        container.innerHTML += card
-      });
+      if (noticias.length > 0) {
+        noticias.forEach ((noticia, index) => {
+          const card = `
+            <div class="card-noticia">
+              <img src="${noticia.imagem}" alt="${noticia.titulo}">
+              <h2>${noticia.titulo}</h2>
+              <p>${noticia.descricao}</p>
+              <a href="../pages/layout/noticia.html?id=${index}">Leia mais</a>
+            </div>
+          `
+
+          container.innerHTML += card
+        })
+      } else {
+        container.innerHTML = '<h1>Nenhuma notícia disponível para esta categoria.</h1>'
+      }
     } else {
-      console.error ('Nenhuma notícia encontrada para esta categoria.')
+      container.innerHTML = '<h1>Página não encontrada.</h1>'
     }
   })
-  .catch (error => console.error ('Erro ao carregar o JSON:', error))
+  .catch (error => console.error('Erro ao carregar o JSON:', error))
